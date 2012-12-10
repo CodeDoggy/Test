26f4011011b6dc0441aa12a56d6e044498728134
Code Doggy <Doggy@codedoggy.com>
2012-12-10 10:11:34 -0800

    Version 4

--- TestApp/TestApp/Controllers/HomeController.cs
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Version 3";
+            ViewBag.Message = "Version 4";
 
             return View();
         }

52aad0b909bed0c2b8e0a547f912b3276ce33ddb
Code Doggy <Doggy@codedoggy.com>
2012-12-10 10:14:15 -0800

    Version 7

--- TestApp/TestApp/Controllers/HomeController.cs
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Version 6";
+            ViewBag.Message = "Version 7";
 
             return View();
         }

b8965b9bdb7dcb0d62a16a0280ecd2f074b05426
unknown <bbredenkamp@Apollo.arminsight.local>
2012-12-10 10:02:21 -0800

    Version 2

--- TestApp/TestApp/Controllers/HomeController.cs
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Version 1";
+            ViewBag.Message = "Version 2";
 
             return View();
         }
